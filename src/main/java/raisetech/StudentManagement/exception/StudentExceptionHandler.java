ee36933be3626ce772a15c53ad3ea7d075106bca
m0809a <boodypoison0@gmail.com>
2025-12-19 12:06:05 +0900

    feat:Validationの例外をユーザー側に出力できるように変更

--- src/main/java/raisetech/StudentManagement/exception/StudentExceptionHandler.java
+++ src/main/java/raisetech/StudentManagement/exception/StudentExceptionHandler.java
@@ -1,7 +1,10 @@
 package raisetech.StudentManagement.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,4 +17,12 @@ public class StudentExceptionHandler {
         .body(e.getMessage());
   }
 
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> handleValidationError(MethodArgumentNotValidException e){
+    String message = e.getBindingResult()
+        .getFieldError()
+        .getDefaultMessage();
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(message);
+  }
 }
