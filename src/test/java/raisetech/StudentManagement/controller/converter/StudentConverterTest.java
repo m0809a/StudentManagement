9de8b66d77c0be6898e549fff2d0ef29a0c8c028
m0809a <boodypoison0@gmail.com>
2026-02-05 14:33:50 +0900

    feat:Converterテストの実装

---
+++ src/test/java/raisetech/StudentManagement/controller/converter/StudentConverterTest.java
@@ -0,0 +1,44 @@
+package raisetech.StudentManagement.controller.converter;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
+
+class StudentConverterTest {
+
+  private StudentConverter sut;
+
+  @BeforeEach
+  void before(){sut = new StudentConverter();}
+
+  @Test
+  void StudentとStudentCourseがStudentDetailに変換されて正しくかえること() throws Exception{
+    Student student = new Student();
+    student.setId("S999999");
+    student.setName("テスト");
+
+    StudentCourse course = new StudentCourse();
+    course.setId("C999999");
+    course.setStudentId("S999999");
+
+    List<StudentDetail> result = sut.convertStudentDetails(
+        List.of(student),
+        List.of(course)
+    );
+
+    assertEquals(1, result.size());
+
+    StudentDetail detail = result.get(0);
+    assertEquals(student, detail.getStudent());
+    assertEquals(1, detail.getStudentsCourseList().size());
+    assertEquals(course, detail.getStudentsCourseList().get(0));
+
+
+  }
+
+}
